d1011c8433edf79763b2b51d34807301fa8eb8c2
Egga (Jan Hartung) <github@egga.de>
2015-11-05 15:38:24 +0100

    nuke

--- settings/fileTemplates/code/TestNG Test Method.java
+++
@@ -1,6 +0,0 @@
-@org.testng.annotations.Test
-public void should${NAME}() {
-  ${BODY}
-  
-  org.hamcrest.MatcherAssert.assertThat(1, org.hamcrest.Matchers.is(org.hamcrest.Matchers.equalTo(1)));
-}
